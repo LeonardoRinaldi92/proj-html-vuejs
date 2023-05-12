b2a349cc084dd3343c9cec5f5d3b043030df74cf
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-12 13:58:24 +0200

    loghetto maledetto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script >
   import topHeader from './components/topHeader.vue';
+  import jumboTron from './components/jumboTron.vue';
   export default {
     name: 'App',
     components: {
       topHeader,
+      jumboTron,
     }
   }
 
@@ -11,10 +13,17 @@
 
 <template>
   <header class="container-fluid">
-    <div class="w-75 m-auto py-3">  
+    <div class="m-auto py-3" style="width: 60%;">  
       <topHeader/>
     </div>
   </header>
+  <main>
+    <section class="jumbo w-100">
+      <jumboTron/>
+
+    </section>
+
+  </main>
 </template>
 
 <style lang="scss">
@@ -22,6 +31,10 @@
   @import "./style/partials/_variables";
   header{
     background-color: $darkblue;
+  }
+  .jumbo {
+    height: 90vh;
+    background-image: url(images/bg-9.jpg);
   }
 
 </style>
---
+++ src/components/jumboTron.vue
@@ -0,0 +1,49 @@
+<script>
+    export default {
+        name: 'jumboTron'
+    }
+</script>
+<template>
+    <div class="m-auto py-4" style="width: 60%">
+        <nav class="d-flex w-100 justify-content-between">
+            <div>
+                <span class="startlogo">
+                    <b>
+                        nex
+                    </b>
+                </span>
+                <span style="margin-left: -10px;">
+                    <b>
+                        gen
+                    </b>
+                </span>
+            </div>
+            <div>
+
+            </div>
+
+        </nav>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @import "../style/partials/_variables";
+    span {
+        color: white;
+        text-transform: uppercase;
+        font-size: 1.3rem;
+        letter-spacing: 5px;
+
+    }
+    .startlogo{
+            padding: 8px 0px 8px 25px;
+            display: inline-block;
+            color: $skytext;
+            background-color: $sky;
+            border-radius: 50px 0px 0px 50px;
+        }
+
+
+
+</style>
