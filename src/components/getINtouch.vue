a1a23a8d8b1974816d8f3dc91fc34d2e971a1b13
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-14 15:55:09 +0200

    struttura form ok

--- src/components/getINtouch.vue
+++ src/components/getINtouch.vue
@@ -30,33 +30,80 @@
                         We will respond to your message soon as possible.
                     </p>
                     <div>
-                        <div class="row gx-2">
+                        <div class="row gx-3 mt-5">
                             <div class="col-xl-6 col-12">
                                 <input type="text" placeholder="name" >
                             </div>
                             <div class="col-xl-6 col-12">
                                 <input type="email" placeholder="email">
                             </div>
-                            <div class="col-xl-6 col-12 mt-2">
+                            <div class="col-xl-6 col-12 mt-3">
                                 <input type="tel" placeholder="phone">
                             </div>
-                            <div class="col-xl-6  col-12">
-                                <select name="cosavuoi" id="" class="mt-2" > 
+                            <div class="col-xl-6  col-12 mt-2">
+                                <select name="cosavuoi" id="tipodiAiuto" class="mt-2" > 
                                     <option value="volvo">more info</option>
                                     <option value="saab">assistance</option>
                                     <option value="mercedes">track</option>
                                     <option value="audi">ciao leo</option>
                                 </select>
                             </div>
-
+                            <div class="col-12 mt-3" >
+                                <textarea name="" id="" cols="98" rows="7" placeholder="Message"></textarea>
+                            </div>
+                            <div class="col-2 mt-3">
+                                <button class="button" type="submit">
+                                    send
+                                </button>
+                            </div>
                         </div>
-                    
-
                     </div>
                 </div>
-                
                 <div class="col-4">
-
+                    <span>
+                        <b>
+                            Example Inc.
+                        </b>
+                    </span>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                    <p>
+                        Ad modi, rem, mollitia molestias laudantium vero ab dolorem iste est iusto.
+                    </p>
+                    <div class="d-flex align-items-end">
+                        <div>
+                            <i class="fa-solid fa-phone round-icon"></i>
+                        </div>
+                        <div class="ms-3">
+                            <p>
+                                +1 (305) 1234-5678
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-end mt-3">
+                        <div>
+                            <i class="fa-solid fa-envelope round-icon"></i>
+                        </div>
+                        <div class="ms-3">
+                            <p>
+                                hello@emaple.com
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-end mt-3">
+                        <div>
+                            <i class="fa-solid fa-location-dot round-icon"></i>
+                        </div>
+                        <div class="ms-3">
+                            <p>
+                                Main Avenue, 987
+                            </p>
+                        </div>
+                    </div>
+                    <button class="reverseButton">
+                        view map
+                    </button>
                 </div>
             </div>
 
@@ -80,21 +127,30 @@
         }
      }
 
+     textarea {
+            padding-top: 15px;
+        }
      input,
      select {
+        height: 70px;
+
+     }
+
+     input,
+     select,
+     textarea {
         width: 100%;
-        height: 50px;
         border: none;
         background-color: $lightGrey;
         border-radius: 5px;
-        font-size: 1.2rem;
+        font-size: 1.5rem;
         padding-left: 15px;
         color: $grey;
         text-transform: capitalize;
         &::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
           color: $grey;
           opacity: 1;
-          font-size: 1.2rem;
+          font-size: 1.5rem;
           text-transform: capitalize; /* Firefox */
         }
 
@@ -110,12 +166,64 @@
           text-transform: capitalize;
         }
 
+
+
+
         option {
             color: $grey;
           font-size: 1.2rem;
           text-transform: capitalize;
-        }
-     }
 
+        }
+
+        
+     }
+     .button {
+            color: white;
+            text-transform: uppercase;
+            border: none;
+            height: 60px;
+            width: 120px;
+            font-size: 1.4rem;
+            margin-top: 20px;
+        }
+    .reverseButton {
+        margin-top: 50px;
+        text-transform: uppercase;
+        color: $blue;
+        height: 60px;
+        width: 180px;
+        font-size: 1.4rem;
+    }
+    .col-4 {
+        padding-left: 60px;
+        span {
+            font-size: 2rem;
+        }
+        p {
+            font-size: 1.3rem;
+            margin-top: 30px;
+            color: $grey;
+        }
+        .round-icon {
+                font-size: 2rem;
+                color: $blue;
+                background-color: $sky;
+                width: 4rem;
+                aspect-ratio: 1;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-radius: 50%;
+            }
+
+        .d-flex {
+            p {
+                font-size: 1.5rem;
+                color: $blue;
+                line-height: 2rem;
+            }
+        }
+    }
 
 </style>
